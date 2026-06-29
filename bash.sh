563354e399da93b6ec6ec03bf8815ef0198c83e1
Seriki Walter Yakub <sandraisom@icloud.com>
2026-06-29 13:24:00 +0100

    Create bash.sh

---
+++ bash.sh
@@ -0,0 +1,2 @@
+curl https://dropmail.me/api/graphql/af_AQFqQmJusEKqeszSQa6PbTdKW8DB8HoSZkndCY_o -H 'Content-Type: application/json' \
+ -d '{"query":"mutation {introduceSession {id, expiresAt, addresses {fil1davykag3@mail2me.co}}}"}'
